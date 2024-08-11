9709f6c27a69ec66eafe2d3dc527daa86182753b
moadhamousti <moadhamousti@gmail.com>
2024-08-11 18:03:38 +0100

    Update

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const app = express();
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -13,20 +14,36 @@ mongoose.connect(process.env.MONGO_URL, {
 .catch((err) => console.log('Database not connected', err));
 
 // Middlewares
-app.use(express.json({ limit: '50mb' })); // Increase payload limit
-app.use(express.urlencoded({ limit: '50mb', extended: true })); // Increase payload limit
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
-// Configure CORS middleware
+// CORS Configuration
+const allowedOrigins = process.env.NODE_ENV === 'production'
+    ? [
+        process.env.ORIGIN_1, 
+        process.env.ORIGIN_2, 
+        process.env.ORIGIN_3 // Add the third origin here
+      ]
+    : ['http://localhost:5173']; // Local development
+
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with the origin of your frontend application
-    credentials: true // Allow credentials
+    origin: (origin, callback) => {
+        console.log('Origin:', origin); // Debugging: Log the origin
+        if (allowedOrigins.includes(origin) || !origin) { // Allow requests with no origin (e.g., Postman)
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/courses', require('./routes/coursesRoute'));
-app.use('/api/form', require('./routes/formRoutes')); // Make sure this matches your API calls
+app.use('/api/form', require('./routes/formRoutes'));
 app.use('/', require('./routes/dashboardRoutes'));
 app.use('/api', require('./routes/userRoutes'));
 
