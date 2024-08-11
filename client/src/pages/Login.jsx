39c8e7bbd4a60bc344add4d2276215d4732b2f94
moadhamousti <moadhamousti@gmail.com>
2024-08-11 18:21:57 +0100

    Update

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -49,7 +49,7 @@ const Login = () => {
   
     const { email, password } = data;
     try {
-      const response = await axios.post('http://localhost:8000/api/auth/login', {
+      const response = await axios.post('https://e-learning-qk1g.onrender.com/api/auth/login', {
         email,
         password
       }, {
